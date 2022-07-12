9a7f91263b3575f69cfbc3fa23f030a9a75f094f
Lucas Boti <luke.boti.lorken@gmail.com>
2022-07-12 16:48:49 -0300

    added tx primary and secondary and bg-primary and secondary. Created devDependecies on package.json file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from "react"
 import NavItem from "./NavItem"
+import logo from "../assets/logo.png"
+import "./nav.css"
 
 function Navbar() {
     return (<>
         <div className="nav-container">
-            <nav className="nav">
+            <nav className="navbar">
+                <a href="/">
+                    <img className="logo" alt="logo" src={logo}></img>
+                </a>
                 <ul>
                     <NavItem to="/"></NavItem>
                 </ul>
