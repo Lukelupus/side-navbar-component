7398320e3f8695029a3fa151341931430cfc333b
Lucas Boti <luke.boti.lorken@gmail.com>
2022-07-12 17:27:02 -0300

    added ReadMe

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react"
-import logo from "../assets/logo.png"
+import brand from "../assets/logo.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 
 
 function Navbar() {
@@ -7,12 +9,12 @@ function Navbar() {
         <div className="nav-container">
             <nav className="navbar">
                 <a href="/">
-                    <img className="logo" alt="logo" src={logo}></img>
+                    <img className="logo" alt="logo" src={brand}></img>
                 </a>
                 <ul>
                     <li className="nav-item">
                     <a href="/" className="nav-link">
-                        
+                    <FontAwesomeIcon icon="fa-solid fa-coffee" size="xs" />
                         <span className="link-text"></span>
                     </a>
                     </li>
